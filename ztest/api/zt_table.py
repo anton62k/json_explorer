c3951eb431cea148d832d5cc975bbe9e663f8286
anton.kashirov <anton.kashirov@gmail.com>
2012-11-18 15:27:50 +0400

    table test

--- ztest/api/zt_table.py
+++ ztest/api/zt_table.py
@@ -35,5 +35,35 @@ class TableTest(BaseCase):
         self.eq(len(self.table.keys()), 2)
 
         self.neq(self.table.add('test'), None)
-        self.eq(len(self.table.keys()), 2)
+        self.eq(len(self.table.keys()), 3)
 
+        self.eq(self.table.add('test'), None)
+        self.eq(len(self.table.keys()), 3)
+
+    def test_remove(self):
+        doc = self.table.add('test')
+        self.eq(len(self.table.keys()), 1)
+        self.eq(self.table.get('test'), doc)
+
+        doc2 = self.table.add('test2')
+        self.eq(len(self.table.keys()), 2)
+        self.eq(self.table.get('test2'), doc2)
+
+        self.eq(self.table.remove('test'), doc)
+        self.eq(self.table.get('test'), None)
+        self.eq(len(self.table.keys()), 1)
+
+        self.eq(self.table.remove('test2'), doc2)
+        self.eq(self.table.get('test2'), None)
+        self.eq(len(self.table.keys()), 0)
+
+    def test_remove_all(self):
+        self.table.add('test1')
+        self.table.add('test2')
+
+        self.table.remove_all()
+
+        self.eq(self.table.get('test1'), None)
+        self.eq(self.table.get('test2'), None)
+
+        self.eq(len(self.table.keys()), 0)
